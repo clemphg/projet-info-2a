fd678552537f5b5e54833be9fe1d0e69bdebb0c7
Clementine Phung <clementine@phung.fr>
2022-10-24 17:06:55 +0200

    ajout vues joueurs (pas complet)

---
+++ vues/joueur/vue_inscription_partie_joueur.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+from PyInquirer import Separator, prompt
+
+
+from vues.abstract_vue import AbstractVue
+
+class VueInscriptionPartieJoueur(AbstractVue):
+    def __init__(self) -> None:
+        self.__questions = [
+            {
+                'type': 'list',
+                'name': 'choix',
+                'message': 'Sélectionner un choix',
+                'choices': [
+                    'Retourner au menu principal'
+                ]
+            }
+        ]
+
+    def display_info(self):
+        pass
+
+    def make_choice(self):
+        reponse = prompt(self.__questions)
---
+++ vues/joueur/vue_principale_joueur.py
@@ -0,0 +1,47 @@
+from pprint import pprint
+
+from PyInquirer import Separator, prompt
+
+from vues.abstract_vue import AbstractVue
+from vues.joueur.vue_creation_personnage_joueur import VueCreationPersonnageJoueur
+from vues.joueur.vue_inscription_partie_joueur import VueInscriptionPartieJoueur
+from vues.joueur.vue_personnages_joueur import VuePersonnagesJoueur
+from vues.joueur.vue_parties_joueur import VuePartiesJoueur
+from vues.joueur.vue_notifs_joueur import VueNotificationsJoueur
+
+class VuePrincipaleJoueur(AbstractVue):
+    def __init__(self) -> None:
+        self.__questions = [
+            {
+                'type': 'list',
+                'name': 'choix',
+                'message': 'Sélectionner un sous-menu',
+                'choices': [
+                    'Créer un personnage',
+                    "M'inscrire à une partie",
+                    'Mes personnages',
+                    'Mes parties',
+                    'Mes notifications',
+                    'Me déconnecter'
+                ]
+            }
+        ]
+
+    def display_info(self):
+        pass
+
+    def make_choice(self):
+        reponse = prompt(self.__questions)
+        if reponse['choix'] == 'Créer un personnage' :
+            return VueCreationPersonnageJoueur()
+        elif reponse['choix'] == "M'inscrire à une partie":
+            return VueInscriptionPartieJoueur()
+        elif reponse['choix'] == 'Mes personnages':
+            return VuePersonnagesJoueur()
+        elif reponse['choix'] == 'Mes parties':
+            return VuePartiesJoueur()
+        elif reponse['choix'] == 'Mes messages':
+            return VueNotificationsJoueur()
+        else:
+            pass
+

da870b40b39810f521df1aac6016658e00d0adbc
Clementine Phung <clementine@phung.fr>
2022-10-28 08:39:12 +0200

    modif vue personnages joueurs pour changer plusieurs fois la classe d'un perso

--- vues/joueur/vue_personnages_joueur.py
+++ vues/joueur/vue_personnages_joueur.py
@@ -27,13 +27,6 @@ class VuePersonnagesJoueur(AbstractVue):
             },
             {
                 'type': 'list',
-                'name': 'choix_perso',
-                'message': 'Sélectionner un personnage',
-                'choices':
-                    ["ID : "+str(perso.id)+" (nom : "+perso.nom+", classe : "+perso.classe+")" for perso in Session().utilisateur.personnages]
-            },
-            {
-                'type': 'list',
                 'name': 'choix_nvlle_classe',
                 'message': 'Sélectionner une nouvelle classe',
                 'choices':
@@ -54,14 +47,31 @@ class VuePersonnagesJoueur(AbstractVue):
         if Session().utilisateur.personnages:
             reponse = prompt(self.__questions[0])
 
-            if reponse['choix_menu'] == "Modifier la classe de l'un de mes personnages":
-                reponse = prompt(self.__questions[1:3])
-                id = reponse['choix_perso'].split(' ')[2]
+            while reponse['choix_menu'] == "Modifier la classe de l'un de mes personnages":
+                # question choix perso définie ici pour que les caractéristiques soient à jour ds les questions si jamais on change plusieurs classes
+                rep_perso = prompt(
+                    {
+                'type': 'list',
+                'name': 'choix_perso',
+                'message': 'Sélectionner un personnage',
+                'choices':
+                    ["ID : "+str(perso.id)+" (nom : "+perso.nom+", classe : "+perso.classe+")" for perso in Session().utilisateur.personnages]
+                })
+                rep_classe = prompt(self.__questions[1])
+                id = rep_perso['choix_perso'].split(' ')[2]
                 for perso in Session().utilisateur.personnages:
                     if perso.id == int(id):
                         cur_perso = perso
-                        DAO().maj_classe(cur_perso, reponse['choix_nvlle_classe'])
-                cur_perso.classe = reponse['choix_nvlle_classe']
+                        DAO().maj_classe(cur_perso, rep_classe['choix_nvlle_classe'])
+                cur_perso.classe = rep_classe['choix_nvlle_classe']
+
+                # on affiche la nouvelle liste des personnages
+                for perso in Session().utilisateur.personnages:
+                    print("ID : ",perso.id,"\nNom :",perso.nom,"\nAge :",perso.age,"\nNiveau :",perso.niveau,"\nRace :",perso.race,"\nClasse :",perso.classe,"\n")
+
+                # on repose la question : modifier classe ou retour au menu principal
+                reponse = prompt(self.__questions[0])
+
 
         from vues.joueur.vue_principale_joueur import VuePrincipaleJoueur
         return VuePrincipaleJoueur()

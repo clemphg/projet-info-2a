67963711e792282bb9e8737c77311002dd956c8c
Amina BELKEBIR-MEKKI <amina.belkebir-mekki@eleve.ensai.fr>
2022-10-29 19:00:37 +0000

    Ajout documentation classe personnage

--- objets_metiers/personnage.py
+++ objets_metiers/personnage.py
@@ -1,16 +1,11 @@
-"""Classe Personnage
 
-Un personnage est créé par un Joueur. Celui-ci en a au maximum trois.
-
-"""
 
 class Personnage():
-
-    def __init__(self, id=None, nom=None, age=None, race=None, niveau=None, classe=None) -> None:
-        """Créer un personnage
-
-        Parameters
+    ''' Un personnage est créé par un joueur et est choisi par ce même joueur lorsqu'il participe à une partie de jeu de rôle, il en a au maximum 3
+        Attributes
         ----------
+        id: int
+           Id du personnage 
         nom : str
             Nom du personnage
         age : int
@@ -21,7 +16,18 @@ class Personnage():
             Niveau du personnage
         classe : str
             Classe du personnage
-        """
+
+        Examples
+        ----------
+        Exemple d'utilisation
+        >>> c = Personnage()
+        >>> c.id.classe()
+
+        '''
+
+    def __init__(self, id=None, nom=None, age=None, race=None, niveau=None, classe=None) -> None:
+        ''' Constructeur'''
+       
         self.__id = id
         self.__nom = nom
         self.__age = age
@@ -31,6 +37,7 @@ class Personnage():
 
     @property
     def id(self):
+        ''' Retourne l'id du personnage'''
         return self.__id
 
     @id.setter
@@ -39,22 +46,27 @@ class Personnage():
 
     @property
     def nom(self):
+        ''' Retourne le nom du personnage'''
         return self.__nom
 
     @property
     def age(self):
+        ''' Retourne l'âge du personnage'''
         return self.__age
 
     @property
     def race(self):
+        ''' Retourne la race du personnage'''
         return self.__race
 
     @property
     def niveau(self):
+        '''Retourne le niveau du personnage'''
         return self.__niveau
 
     @property
     def classe(self):
+        ''' Retourne la classe du personnage'''
         return self.__classe
 
     @classe.setter

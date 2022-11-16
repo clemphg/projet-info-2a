6e8f2ab117c39eb696836be42f3d4e24d156ee7e
Amina BELKEBIR-MEKKI <amina.belkebir-mekki@eleve.ensai.fr>
2022-11-16 17:20:36 +0000

    Documentation Vue Notifications

--- vues/joueur/vue_notifications_joueur.py
+++ vues/joueur/vue_notifications_joueur.py
@@ -11,6 +11,7 @@ from service.service_messages import ServiceMessages
 
 class VueNotificationsJoueur(AbstractVue):
     def __init__(self) -> None:
+        " Création d'une vue Notifications avec la définition d'une variable questions qui va pouvoir indiquer les actions du joueur lorsqu'il est sur la vue en question. En l'occurrence ici, le joueur peut décider de rester sur la page Notifications ou retourner au menu principal en sélectionnant 'Retourner au menu principal'"
         self.__questions = [
             {
                 'type': 'list',
@@ -23,6 +24,7 @@ class VueNotificationsJoueur(AbstractVue):
         ]
 
     def display_info(self):
+        "Permet d'afficher sur la console les différentes notifications du joueur avec la date de celles-ci. S'il n'a aucun message, il sera affiché sur la console le message suivant : 'Pas de notifications'"
         messages = ServiceMessages().chercher_messages(Session().utilisateur.pseudo)
         if messages:
             for message in messages:
@@ -31,6 +33,7 @@ class VueNotificationsJoueur(AbstractVue):
             print("Pas de notifications\n")
 
     def make_choice(self):
+        "Permet au joueur de retourner sur son menu principal s'il le souhaite/en fait le choix"
         reponse = prompt(self.__questions)
         if reponse['choix']=='Retourner au menu principal':
             from vues.joueur.vue_principale_joueur import VuePrincipaleJoueur

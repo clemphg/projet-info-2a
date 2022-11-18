ad0ea07bbaeac2043f263ab14ac188a4d3bbd574
Amina BELKEBIR-MEKKI <amina.belkebir-mekki@eleve.ensai.fr>
2022-11-18 09:43:00 +0000

    Update organisateur.py

--- objets_metiers/organisateur.py
+++ objets_metiers/organisateur.py
@@ -1,4 +1,3 @@
-
 class Organisateur():
     ''' Organisteur du jeu, il a beaucoup de pouvoir car il peut supprimer des personnages, bannir des joueur etc
     Attributes
@@ -14,23 +13,3 @@ class Organisateur():
     @property
     def pseudo(self):
         return self.__pseudo
-
-    def supprimer_personnage(self, personnage, partie):
-        partie.supprimer(personnage)
-
-    def reaffecter_personnage(self, personnage, prec_partie, nvlle_partie):
-        # si on peut l'ajouter à la nouvelle partie,
-        pass
-
-    def bannir_joueur(self, joueur):
-        # effacer le joueur, ses personnages et l'enlever de toutes les parties
-        pass
-
-    def bannir_mj(self, mj):
-        # effacer le mj, ses scénarios et toutes les parties qu'il menaient
-        # => supprimer les joueurs de ces parties
-        pass
-
-    def supprimer_partie(self, partie):
-        # supprimer le maitre de jeu et tous les joueurs de la partie
-        pass

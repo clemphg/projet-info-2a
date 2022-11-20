b6ad72fe83cc9ac50834d6bb65c6b1cbef0c65a6
Amina BELKEBIR-MEKKI <amina.belkebir-mekki@eleve.ensai.fr>
2022-11-20 16:38:33 +0000

    Update vue maître de jeu

--- vues/maitre_de_jeu/vue_creation_partie_mj.py
+++ vues/maitre_de_jeu/vue_creation_partie_mj.py
@@ -13,6 +13,7 @@ from objets_metiers.partie import Partie
 
 class VueCreationPartieMJ(AbstractVue):
     def __init__(self) -> None:
+        "Création de la vue avec la définition d'une variable questions qui va stocker toutes les intéractions du maître de jeu. Ici, il va pouvoir choisir de sélectionner un scénario et créer une partie en validant la sélection, il peut abandonner et même retourner au menu principal. "
         self.__questions = [
             {
                 'type': 'list',
@@ -45,9 +46,11 @@ class VueCreationPartieMJ(AbstractVue):
         ]
 
     def display_info(self):
+        " Affiche 'Créer une nouvelle partie '"
         print("--- Créer une nouvelle partie ---")
 
     def make_choice(self):
+        " Le maître du jeu va sélectionner un ou plusieurs créneaux parmi ses créneaux de libre. S'il est possible pour lui de créer une nouvelle partie sur les créneaux choisis, il aura le choix de sélectionner un de ses scénarios, de le valider et de créer donc une nouvelle partie, s'affichera doncle message suivant:'Partie créée' . Mais il pourra également abandonner son processus de création de partie et donc s'affichera sur l'écran 'Abandon'. Sinon, il peut tout à fait faire le choix de retourner au menu principal, la vue principale sera retournée. Dans le cas où il lui est impossible de créer une nouvelle partie sur les créneaux choisis, le message suivant s'affichera: 'Ce créneau est déjà complet, veuillez en choisir un autre.' et il pourra faire le choix de retourner au menu principal, dans ce cas-là sera retournée sa vue principale. Dans le cas où il n'a pas de créneaux disponibles, il sera affiché le message suivant: 'Vous n'avez pas de disponibilité.\nVous pouvez vous désinscrire (i.e. supprimer) une partie via le menu 'Mes parties'' et il pourra faire le choix de retourner au menu prncipal: la vue principale sera retournée "
         creneaux_dispo = ServiceMaitreDeJeu().liste_creneaux_dispos(Session().utilisateur)
         if len(creneaux_dispo)>0:
             reponse = prompt(

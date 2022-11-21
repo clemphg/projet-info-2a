9afb21caf29663be51326889b5dbf09afa940b3f
Amina BELKEBIR-MEKKI <amina.belkebir-mekki@eleve.ensai.fr>
2022-11-21 11:16:37 +0000

    Update vue_creation_scenario_mj.py

--- vues/maitre_de_jeu/vue_creation_scenario_mj.py
+++ vues/maitre_de_jeu/vue_creation_scenario_mj.py
@@ -14,6 +14,7 @@ from objets_metiers.scenario import Scenario
 
 class ValidationNom(Validator):
     def validate(self, document):
+        " Permet de vérifier que le texte rentré ici défini comme document ait un nombre de caractères entre 5 et 25 caractères, sans caractères spéciaux, et affiche un message d'erreur si ce n'est pas le cas"
         ok = regex.match("^[A-Za-z'eèàù ]{5,25}$", document.text)
         if not ok:
             raise ValidationError(
@@ -23,6 +24,7 @@ class ValidationNom(Validator):
 
 class ValidationDescription(Validator):
     def validate(self, document):
+        " Permet de vérifier que le texte rentré ici défini comme document n'ait pas de caractères spéciaux qui ne seraient pas autorisés, et affiche un message d'erreur si ce n'est pas le cas"
         ok = regex.match("^[A-Za-z.,;!?' êîûéèàù]{0,200}$", document.text)
         if not ok:
             raise ValidationError(
@@ -32,6 +34,7 @@ class ValidationDescription(Validator):
 
 class ValidationEntier(Validator):
     def validate(self, document):
+        " Permet de vérifier que le texte rentré ici défini comme document soit bien un nombre entier sinon s'affiche un message d'erreur."
         ok = regex.match("^\d{1,}$", document.text)
         if not ok:
             raise ValidationError(
@@ -41,6 +44,7 @@ class ValidationEntier(Validator):
 
 class VueCreationScenarioMJ(AbstractVue):
     def __init__(self) -> None:
+        "Création d'une vue Création Scénario avec la défintion d'une variable questions, qui va stocker l'ensemble des intéractions du maître de jeu. Il pourra choisir de créer un scénario et valider le nom du scénario, le niveau minimum recquis des personnages ainsi que la description du scénario. Le maître de jeu peut également choisir d'abandonner la création de scénario et/ou retourner sur son menu principal. "
         self.__questions = [
             {
                 'type': 'input',
@@ -80,9 +84,11 @@ class VueCreationScenarioMJ(AbstractVue):
         ]
 
     def display_info(self):
+        "Affiche sur l'écran 'Création d'un scénario'"
         print("Création d'un scénario.\n")
 
     def make_choice(self):
+        "Permet d'afficher le menu à partir de la variable questions. Ce qui s'affichera dépendra des choix du maître de jeu. Si celui-ci décide de créer son scénario, il pourra le faire à condition qu'il ait validé le nom de son scénario, la description correspondante et le niveau minimum requis pour les personnages. Il s'affichera un message comme quoi le scénario a bien été créé. De plus, le scénario nouvellement créé sera stocké dans la base de données. Dans le cas où il a plus de deux scénarions, il sera affiché un message comme quoi le maître de jeu ne peut pas créer un nouveau scénario car il en a dejà deux. Finalement, il lui sera proposé de retourner au menu principal, et sera donc retournée sa vue principale.'"
         if len(Session().utilisateur.scenarios)<2:
             reponses = prompt(self.__questions[0:4])
 
